a4d4c5569c759ae63e02fe06830b8f65a88ea5cb
Oorjit Chowdhary <oorjit.chowdhary@hotmail.com>
2025-02-15 02:17:55 -0800

    init node

---
+++ server/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+
+require('dotenv').config();
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, err => {
+    if (err) throw err;
+    console.log(`Server is running on port ${port}`);
+});
