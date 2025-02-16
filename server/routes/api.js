3ecc2f6b3a8cf6bf25a39e5155e7d4433d8956c8
Oorjit Chowdhary <oorjit.chowdhary@hotmail.com>
2025-02-15 16:40:45 -0800

    elevenlabs and supabase bucket

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,10 +1,14 @@
 require('dotenv').config();
+const { createWriteStream, readFileSync, unlink } = require('fs');
+const { v4: uuidv4 } = require('uuid');
 
 const router = require('express').Router();
 const Groq = require('groq-sdk');
+const { ElevenLabsClient, play } = require('elevenlabs');
+const { createClient: createSupabaseClient } = require('@supabase/supabase-js');
+const supabase = createSupabaseClient(process.env.SUPABASE_URL, process.env.SUPABASE_SERVICE_KEY);
 
 const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
-
 router.get('/story', async (req, res) => {
     const { genre } = req.query;
     const response = await groq.chat.completions.create({
@@ -37,6 +41,54 @@ Your sentences are short and easy to understand for the defined age bracket of c
     });
 
     return res.json(response);
+});
+
+const client = new ElevenLabsClient({ apiKey: process.env.ELEVENLABS_API_KEY });
+router.get("/voice", async (req, res) => {
+    try {
+        const { text } = req.query;
+        if (!text) {
+            return res.status(400).json({ error: "Text query parameter is required" });
+        }
+
+        const audio = await client.textToSpeech.convert("RLZSZodi7ECE8WCeD9Pp", {
+            text,
+            model_id: "eleven_multilingual_v2",
+            output_format: "mp3_44100_128",
+        });
+
+        const fileName = `${uuidv4()}.mp3`;
+        const fileStream = createWriteStream(fileName);
+
+        audio.pipe(fileStream);
+
+        fileStream.on("finish", async () => {
+            try {
+                const fileBuffer = readFileSync(`./${fileName}`);
+                const { data, error } = await supabase.storage.from("narrator-audio-files").upload(fileName, fileBuffer, {contentType: "audio/mpeg"});
+                unlink(fileName, () => console.log("Deleted audio file"));
+
+                if (error) {
+                    console.error("Error uploading audio file:", error);
+                    return res.status(500).json({ error: "Failed to upload audio file" });
+                }
+
+                return res.json({ url: data.fullPath });
+            } catch (uploadError) {
+                console.error("Error uploading audio file:", uploadError);
+                res.status(500).json({ error: "Failed to upload audio file" });
+            }
+        });
+
+        fileStream.on("error", (err) => {
+            console.error("Error writing file:", err);
+            res.status(500).json({ error: "Failed to write audio file" });
+        });
+
+    } catch (error) {
+        console.error("Error generating speech:", error);
+        res.status(500).json({ error: "Failed to generate speech" });
+    }
 });
 
 module.exports = router;
